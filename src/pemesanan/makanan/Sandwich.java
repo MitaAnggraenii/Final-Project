e76db4a8a0f8f0fa5f8ffa7c42bc2391cf818639
della1917051056 <62734974+della1917051056@users.noreply.github.com>
2021-01-20 04:35:18 -0800

    Update Sandwich.java

--- src/pemesanan/makanan/Sandwich.java
+++ src/pemesanan/makanan/Sandwich.java
@@ -1,1 +1,10 @@
+package pemesanan.makanan;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Sandwich extends Makanan {
+   private IntegerProperty jumlah;
+   
+     public Sandwich(int ID,int Jumlah) {
+        super(ID,Jumlah);
