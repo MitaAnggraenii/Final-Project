42ffa0deccf45202a61634ee5e1e26eb471a4445
della1917051056 <62734974+della1917051056@users.noreply.github.com>
2021-01-20 04:38:15 -0800

    Update Makanan.java

--- src/pemesanan/makanan/Makanan.java
+++ src/pemesanan/makanan/Makanan.java
@@ -1,1 +1,19 @@
+package pemesanan.makanan;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public abstract class Makanan {
+    private StringProperty Nama;
+    private IntegerProperty ID,jumlah,Harga,Total;
+    
+    public Makanan(int ID,int Jumlah){
+        this.Nama = new SimpleStringProperty();
+        this.jumlah = new SimpleIntegerProperty(Jumlah);
+        this.Harga = new SimpleIntegerProperty();
+        this.Total = new SimpleIntegerProperty();
+        this.ID = new SimpleIntegerProperty();
+    }
+    
