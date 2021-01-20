784c2f6d13deb452edf109c9f31f92d93bd870fc
Fista1917051066 <62733591+Fista1917051066@users.noreply.github.com>
2021-01-20 15:14:58 +0700

    Update koneksi.java

--- koneksi.java
+++ koneksi.java
@@ -1,1 +1,10 @@
+package pemesanan.makanan;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class koneksi {
+        
+        public static String MYSQLhost = "jdbc:mysql://localhost/pemesanan_makanan";
+        public static String user = "root";
+        public static String pass = "";
