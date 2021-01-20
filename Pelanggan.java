d0d8670de258c11d1b1711877a97c7036987295b
MitaAnggraenii <62734174+MitaAnggraenii@users.noreply.github.com>
2021-01-20 15:32:26 +0700

    Update Pelanggan.java

--- Pelanggan.java
+++ Pelanggan.java
@@ -1,1 +1,28 @@
+package pemesanan.makanan;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+
+public class Pelanggan {
+    private IntegerProperty ID;
+    private StringProperty NamaPelanggan;
+    private Makanan makanan;
+    
+    
+    public Pelanggan(int id, Makanan makanan,String Nama) {
+        this.makanan = makanan;
+        this.ID = new SimpleIntegerProperty (id); 
+        this.NamaPelanggan = new SimpleStringProperty(Nama);
+    }
+    
+    
+    public StringProperty NamaPelangganProperty(){
+        return NamaPelanggan;
+    }
+    
+    public IntegerProperty IDProperty(){
+        return ID;
+    }
