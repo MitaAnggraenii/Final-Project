49e977c04bf9cb5e7b6149e44ecf00b5badd4aaf
Fista1917051066 <62733591+Fista1917051066@users.noreply.github.com>
2021-01-20 15:09:28 +0700

    Update Burger.java

--- Burger.java
+++ Burger.java
@@ -2,3 +2,15 @@ package pemesanan.makanan;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+
+public class Burger extends Makanan {
+    private IntegerProperty jumlah;
+    public Burger(int ID, int Jumlah) {
+        super(ID,Jumlah);
+        this.jumlah = new SimpleIntegerProperty(Jumlah);
+        super.setNama("Burger");
+        super.setHarga(20000);
+        super.TotalH();
+    }
+        
+}
