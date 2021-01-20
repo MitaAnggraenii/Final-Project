cd09c4916eef96cecef4bb50cc8f5dd987d6759f
MitaAnggraenii <62734174+MitaAnggraenii@users.noreply.github.com>
2021-01-20 15:16:41 +0700

    Update Sandwich.java

--- Sandwich.java
+++ Sandwich.java
@@ -1,13 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pemesanan.makanan;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
-/**
- *
- * @author ACER
- */
+public class Sandwich extends Makanan {
+   private IntegerProperty jumlah;
+   
+     public Sandwich(int ID,int Jumlah) {
+        super(ID,Jumlah);
+        this.jumlah = new SimpleIntegerProperty(Jumlah);
+        super.setNama("Sandwich");
+        super.setHarga(15000);
+        super.TotalH();
+    }
+}
+
