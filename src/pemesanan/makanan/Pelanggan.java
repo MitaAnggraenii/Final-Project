5ccc31b50c09897f05a9bf9f1ebac9de820c57f3
della1917051056 <62734974+della1917051056@users.noreply.github.com>
2021-01-20 04:46:16 -0800

    Update Pelanggan.java

--- src/pemesanan/makanan/Pelanggan.java
+++ src/pemesanan/makanan/Pelanggan.java
@@ -1,1 +1,21 @@
+package pemesanan.makanan;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Pelanggan {
+    private IntegerProperty ID;
+    private StringProperty NamaPelanggan;
+    private Makanan makanan;
+    
+    
+    public Pelanggan(int id, Makanan makanan,String Nama) {
+        this.makanan = makanan;
+        this.ID = new SimpleIntegerProperty (id); 
+        this.NamaPelanggan = new SimpleStringProperty(Nama);
+    }
+    
+    
+   
