5a3520978dfd9cda1d84aa469c28cf676f99d30b
Fista1917051066 <62733591+Fista1917051066@users.noreply.github.com>
2021-01-20 19:57:42 +0700

    Update koneksi.java

--- src/pemesanan/makanan/koneksi.java
+++ src/pemesanan/makanan/koneksi.java
@@ -11,3 +11,19 @@ public class koneksi {
         
         public static Connection koneksi(String driver)throws SQLException {
         Connection conn = null;
+        
+        if ("MYSQL".equals(driver)){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(MYSQLhost,user,pass);
+            System.out.println("Koneksi Berhasil");
+            return conn;
+            }
+        catch (SQLException e) {
+            System.out.println("Koneksi Gagal "+e);
+        }
+        catch (ClassNotFoundException e){
+            System.out.println("Koneksi Gagal "+e);
+        }
+       }
+       
