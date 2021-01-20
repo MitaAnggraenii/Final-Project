d9cd7419666786dbfa69d31f022d68baaa091b16
MitaAnggraenii <62734174+MitaAnggraenii@users.noreply.github.com>
2021-01-20 19:55:46 +0700

    Update koneksi.java

--- src/pemesanan/makanan/koneksi.java
+++ src/pemesanan/makanan/koneksi.java
@@ -1,1 +1,13 @@
+package pemesanan.makanan;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class koneksi {
+        
+        public static String MYSQLhost = "jdbc:mysql://localhost/pemesanan_makanan";
+        public static String user = "root";
+        public static String pass = "";
+        
+        public static Connection koneksi(String driver)throws SQLException {
+        Connection conn = null;
