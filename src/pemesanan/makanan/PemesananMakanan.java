4b7c59db490fc428bb72d8791754903c00058e33
Fista1917051066 <62733591+Fista1917051066@users.noreply.github.com>
2021-01-20 19:59:17 +0700

    Update PemesananMakanan.java

--- src/pemesanan/makanan/PemesananMakanan.java
+++ src/pemesanan/makanan/PemesananMakanan.java
@@ -1,1 +1,30 @@
+package pemesanan.makanan;
 
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class PemesananMakanan extends Application {
+    TableView tabel = new TableView();
+    int count = 1;
+    ObservableList<Pelanggan> data = FXCollections.observableArrayList();
+    @Override
+    public void start(Stage primaryStage) {
+        //koneksi.koneksi();
+       tabel.setEditable(true);
+        final TextField txt = new TextField();
+        txt.setPromptText("Nama");
