36a32ed72989fdf3e86307fbdba5a53bf25badfd
della1917051056 <62734974+della1917051056@users.noreply.github.com>
2021-01-22 18:32:18 -0800

    Update PemesananMakananDataModel.java

--- src/pemesanan/makanan/PemesananMakananDataModel.java
+++ src/pemesanan/makanan/PemesananMakananDataModel.java
@@ -45,5 +45,50 @@ public class PemesananMakananDataModel {
         stmtMakanan.execute();
 
     }
+    public void inputDataS(Pelanggan pelanggan) throws SQLException {
+        String insertPelanggan = "INSERT INTO Pelanggan (ID,NamaPelanggan,IDMakanan)"
+                + " VALUES (?,?,?)";
+        String insertMakanan = "INSERT INTO Makanan (id,nama,jumlah)"
+                + " VALUES (?,?,?)";
+        String insertBurger = "INSERT INTO Burger (id,harga,totalharga)"
+                + " VALUES (?,?,?)";
+        String insertSandwich = "INSERT INTO `sandwich` (id,harga,totalharga)"
+                + " VALUES (?,?,?)";
 
-    
+        PreparedStatement stmtPelanggan = CONN.prepareStatement(insertPelanggan);
+        stmtPelanggan.setInt(1, pelanggan.getID());
+        stmtPelanggan.setString(2, pelanggan.getNama());
+        stmtPelanggan.setInt(3, pelanggan.getMakananID());
+        stmtPelanggan.execute();
+        
+        PreparedStatement stmtSandwich = CONN.prepareStatement(insertSandwich);
+        stmtSandwich.setInt(1, pelanggan.getID());
+        stmtSandwich.setInt(2, pelanggan.makanan.getHarga());
+        stmtSandwich.setInt(3, pelanggan.makanan.getTotal());
+        stmtSandwich.execute();
+
+        PreparedStatement stmtMakanan = CONN.prepareStatement(insertMakanan);
+        stmtMakanan.setInt(1, pelanggan.makanan.getID());
+        stmtMakanan.setString(2, pelanggan.makanan.getNama());
+        stmtMakanan.setInt(3, pelanggan.makanan.getJumlah());
+        stmtMakanan.execute();
+
+    }
+
+    public ObservableList getData() throws SQLException {
+        ObservableList<Pelanggan> data = FXCollections.observableArrayList();
+        Burger burger;
+        Sandwich sandwich;
+        
+        String sqlPelanggan = "SELECT * FROM `pelanggan`";
+        String sqlMakanan = "SELECT * FROM `makanan`";
+        String sqlBurger = "SELECT * FROM `burger`";
+        String sqlSandwich = "SELECT * FROM `sandwich`";
+
+
+        try {
+            ResultSet rsPelanggan = CONN.createStatement().executeQuery(sqlPelanggan);
+            ResultSet rsMakanan = CONN.createStatement().executeQuery(sqlMakanan);
+            ResultSet rsBurger = CONN.createStatement().executeQuery(sqlBurger);
+            ResultSet rsSandwich = CONN.createStatement().executeQuery(sqlSandwich);
+            while (rsPelanggan.next() && rsMakanan.next()) {
