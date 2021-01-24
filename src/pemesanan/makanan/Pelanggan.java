3688b91e31d0b8825e11d625cd3fd64b4f5c2a14
della1917051056 <62734974+della1917051056@users.noreply.github.com>
2021-01-23 18:57:26 -0800

    Update Pelanggan.java

--- src/pemesanan/makanan/Pelanggan.java
+++ src/pemesanan/makanan/Pelanggan.java
@@ -5,35 +5,29 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+/**
+ *
+ * @author ACER
+ */
 public class Pelanggan {
     private IntegerProperty ID;
     private StringProperty NamaPelanggan;
-    private Makanan makanan;
+    public Makanan makanan;
+    public Burger burger;
+    public Sandwich sandwich;
     
     
     public Pelanggan(int id, Makanan makanan,String Nama) {
-        this.makanan = makanan;
+        if("Burger".equals(makanan.getNama())){
+            this.burger = (Burger) makanan;
+            this.makanan = this.burger;
+        }
+        else{
+            this.sandwich = (Sandwich) makanan;
+            this.makanan = this.sandwich;
+        }
+        
         this.ID = new SimpleIntegerProperty (id); 
         this.NamaPelanggan = new SimpleStringProperty(Nama);
     }
     
-    public StringProperty NamaPelangganProperty(){
-        return NamaPelanggan;
-    }
-    
-    public IntegerProperty IDProperty(){
-        return ID;
-    }
-
-    public void setNama(String Nama){
-        this.NamaPelanggan.set(Nama);
-    }
-    
-    public void setID(int id){
-        this.ID.set(id);
-    }
-    
-    public int getID(){
-        return ID.get();
-    }
-   
