8d5557feabbe8eed7f0d18cf2b597d3faa34d643
Fista1917051066 <62733591+Fista1917051066@users.noreply.github.com>
2021-01-24 10:06:24 +0700

    Update koneksi.java

--- src/pemesanan/makanan/koneksi.java
+++ src/pemesanan/makanan/koneksi.java
@@ -1,40 +1,11 @@
-package pemesanan.makanan;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class koneksi {
         
-        public static String MYSQLhost = "jdbc:mysql://localhost/pemesanan_makanan";
+        public static String MYSQLhost = "jdbc:mysql://localhost/pemesananmakanan";
         public static String user = "root";
         public static String pass = "";
-        
         public static Connection koneksi(String driver)throws SQLException {
         Connection conn = null;
-        
-        if ("MYSQL".equals(driver)){
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(MYSQLhost,user,pass);
-            System.out.println("Koneksi Berhasil");
-            return conn;
-            }
-        catch (SQLException e) {
-            System.out.println("Koneksi Gagal "+e);
-        }
-        catch (ClassNotFoundException e){
-            System.out.println("Koneksi Gagal "+e);
-        }
-       }
-       else if ("SQLITE".equals(driver)){
-           try {
-                    Class.forName("org.sqlite.JDBC");
-                    conn = DriverManager.getConnection("jdbc:sqlite:D:\\tubes\\sqlite\\PemesananMakanan.sqlite");
-                    return conn;
-                } catch (ClassNotFoundException ex) {
-                    System.out.println("Librari tidak ada");
-                }
-       }
-       return null; 
-    }
-}
